709a4be128c84e64314c8c26bc5ae084fce4aa63
darshan <darsh2605@gmail.com>
2024-04-25 02:00:24 +0530

    minor changes

--- 05_maximum_of_two_number.py
+++ 05_maximum_of_two_number.py
@@ -1,10 +1,10 @@
 # This programs will interchange first and last elements of list 
 
-num_1 = input("Enter Number 1: ")  
+num_1 = int(input("Enter Number 1: ") )
 
-num_2 = input("Enter Number 2: ")  
+num_2 = int(input("Enter Number 2: ")  )
 
-if num_1 > num_2:
+if num_1 >= num_2:
     print(f'Number 1 : {num_1}, Number 2 : {num_2}, Maximum number is {num_1}')
 else:
     print(f'Number 1 : {num_1}, Number 2 : {num_2}, Maximum number is {num_2}')
