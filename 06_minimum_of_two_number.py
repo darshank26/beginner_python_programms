3c5ebfa063dc6def494ed67ded325e1ac0cdae53
darshan <darsh2605@gmail.com>
2024-04-25 02:02:13 +0530

    minimum of two number

---
+++ 06_minimum_of_two_number.py
@@ -0,0 +1,10 @@
+# This programs will interchange first and last elements of list 
+
+num_1 = int(input("Enter Number 1: ") )
+
+num_2 = int(input("Enter Number 2: ")  )
+
+if num_1 <= num_2:
+    print(f'Number 1 : {num_1}, Number 2 : {num_2}, Minimum number is {num_1}')
+else:
+    print(f'Number 1 : {num_1}, Number 2 : {num_2}, Minimum number is {num_2}')
