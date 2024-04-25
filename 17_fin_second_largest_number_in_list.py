d984cb1b3f59340ec756ea5ca2127091c575b5b2
darshan <darsh2605@gmail.com>
2024-04-25 23:24:04 +0530

    second largest num in list

---
+++ 17_fin_second_largest_number_in_list.py
@@ -0,0 +1,18 @@
+input_str = input("Enter elements of the list separated by space: ")  
+
+my_list = input_str.split()  
+my_list = [int(num) for num in my_list]  
+
+print(f'List {my_list}')
+
+# 1. using sort method 
+
+my_list.sort()
+
+print(f'Largest number {my_list[(len(my_list)-2)]}')
+
+
+
+    
+
+
